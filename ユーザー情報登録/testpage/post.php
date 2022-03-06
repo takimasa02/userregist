c6e78e848d3aec21b2cbfb2cf62a3192bb46c444
takimasa02 <101028034+takimasa02@users.noreply.github.com>
2022-03-06 11:11:57 +0900

    Add files via upload

---
+++ ユーザー情報登録/testpage/post.php
@@ -0,0 +1,17 @@
+<?php
+$lastname = $_POST["lastname"];
+$firstname = $_POST["firstname"];
+$lastname_kana = $_POST["lastname_kana"];
+$firstname_kana = $_POST["firstname_kana"];
+$sex = $_POST["sex"];
+$mailaddress = $_POST["mailaddress"];
+$zip_code = $_POST["zip_code"];
+$pref = $_POST["pref"];
+$city = $_POST["city"];
+$address=$_POST["address"];
+$building=$_POST["building"];
+$tel=$_POST["tel"];
+$sample=$_POST["sample"];
+$question=$_POST["question"];
+include "userInfo_regist_comfirm.html"
+?>
---
+++ ユーザー情報登録/testpage/postConftoFix.php
@@ -0,0 +1,5 @@
+<?php
+$lastname = $_POST["lastname"];
+$firstname = $_POST["firstname"];
+include "userInfo_regist_fix.html"
+?>
